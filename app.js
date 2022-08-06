9f53f4682494624b5fe30c0e25944fe881e0dc1d
MariaLauraFlaxMarco <todosepuede_victoria@yahoo.com.ar>
2022-08-05 21:43:33 -0300

    estilos header

--- app.js
+++ app.js
@@ -13,3 +13,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"));
 }) 
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/register.html"));
+})
